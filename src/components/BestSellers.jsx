7bec3ad2f18e8fc324fcbe031b6e7735a79c6d15
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-02-04 21:27:58 -0300

    mudança best sellers

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -20,46 +20,43 @@ const BestSellers = () => {
     <section className='best-sellers pt-10' id='service'>
         <h1 className='mt-40 text-6xl'><strong className="strong">MAIS VENDIDOS</strong></h1>
         <div className="flex">
-                <div className="sellers-box p-4">
-                    <img src={f1ef2} alt=""className='md:w-best-sellers-400 md:h-best-sellers-350 w-80 h-60'/>
-                    <h2 className='leading-8 pt-2'>Impressão de Projetos</h2>
-                    </div>
-                    
-                    <div className="sellers-box md:p-4">
-                    <img src={adesivos} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 h-60'/>
-                    <h2>Etiquetas/Adesivos</h2>
-                    </div>
-                    
-                    <div className="sellers-box p-5">
-                    <img src={env} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-80 h-60'/>
-                    <h2 className='leading-9'>Impressão de Envelopes</h2>
-                    </div>
-                    <div className="sellers-box">
-                    <img src={cdv} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-96 h-60'/>
-                    <h2>Cartão de Visita</h2>
-                    </div>
-                    <div className="sellers-box">
-                    <img src={folder1} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-96 h-60'/>
-                    <h2>Etiquetas Escolares</h2>
-                    </div>
-
-                    <div className="sellers-box">
-                    <img src={tags} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-80 h-60'/>
-                    <h2>Tags</h2>
-                    </div>
-                    <div className="sellers-box">
-                    <img src={ag} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-80 h-60'/>
-                    <h2 className='leading-9'>Agendas Personalizadas</h2>
-                    </div>
-                    <div className="sellers-box">
-                    <img src={enc} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-96 h-60'/>
-                    <h2>Encadernações</h2>
-                    </div>
-                    <div className="sellers-box">
-                    <img src={cvan} alt="" className='md:w-best-sellers-400 md:h-best-sellers-350 w-96 h-60'/>
-                    <h2>Convites</h2>
-                    </div>
-                </div>
+            <div className="sellers-box">
+              <img src={f1ef2} alt="Impressões A1 e A2"/>
+              <h2>Impressão de Projetos</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={adesivos} alt="Adesivos" />
+              <h2>Etiquetas/Adesivos</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={env} alt="Envelopes" />
+              <h2>Impressão em Envelopes</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={cdv} alt="Cartão de Visita" />
+              <h2>Cartão de Visita</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={folder1} alt="Etiquetas" />
+              <h2>Etiquetas Escolares</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={tags} alt="Tags" />
+              <h2>Tags</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={ag} alt="Agendas" />
+              <h2>Agendas Personalizadas</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={enc} alt="Encadernação" />
+              <h2>Encadernações</h2>
+            </div>
+            <div className="sellers-box">
+              <img src={cvan} alt="Convite" />
+              <h2>Convites</h2>
+            </div>
+        </div>
     </section>
   )
 }
